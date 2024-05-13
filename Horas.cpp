f2f87ca310fe7e5d792b56cd7188b0d3b04e33e6
Frankcastro02 <164245539+Frankcastro02@users.noreply.github.com>
2024-05-13 12:58:04 -0600

    Add files via upload

---
+++ Horas.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+    int H = 0;
+    string Nombre;
+    float Salario = 0.0;
+    float Extra = 0.0;
+    float Impuesto = 0.0;
+    cout << "Inserte nombre" << '\n';
+    cin >> Nombre;
+    cout << "Inserte cantidad de horas" << '\n';
+    cin >> H;
+    if (H <= 160)
+    {
+        Salario = H * 10;
+        cout << Nombre << " esta ganando " << "$" << Salario;
+    }
+    else if (H >= 161)
+    {
+        Extra = H - 160;
+        Salario = ((Extra * 15) + 1600);
+
+        if (Salario <= 2000)
+        {
+            cout << "Salario libre de impuestos";
+            cout << Nombre << " esta ganando " << "$" << Salario;
+        }    
+        else if (Salario <= 2200)
+            {
+                Impuesto = Salario * 0.20;
+                Salario = Salario - Impuesto;
+                cout << Nombre << " esta ganando " << "$" << Salario;
+            }
+        else if (Salario >= 2200)
+            {
+                Impuesto = Salario * 0.30;
+                Salario = Salario - Impuesto;
+                cout << Nombre << " esta ganando " << "$" << Salario;
+            }
+    }
+    return 0;
+}
